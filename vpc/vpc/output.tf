02b32af8c8a9f08c1c974fb935f594b9aacd5c31
Alex Treviso <alex.treviso-ext@duratex.com.br>
2022-04-22 09:56:58 -0300

    add vpc module

---
+++ vpc/vpc/output.tf
@@ -0,0 +1,11 @@
+output "private_subnets" {
+  value = aws_subnet.private.*.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public.*.id
+}
+
+output "internet_gateway" {
+  value = aws_internet_gateway.internet_gateway.*.id
+}
